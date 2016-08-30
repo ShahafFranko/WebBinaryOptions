302d743cc0e3a268e494544f4512871d66aaaded
chompicollege <elroie.college@gmail.com>
2016-08-30 17:29:14 +0300

    my fucking commit does not arrived to server

---
+++ BinaryOptions.WebServer/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Security;
+using BinaryOptions.WebServer.Models;
+
+namespace BinaryOptions.WebServer.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                return View("~/Views/Index/Index.cshtml");
+            }
+            
+            return View("~/Views/Login/Login.cshtml");
+        }
+
+        [System.Web.Mvc.HttpPost]
+        public void Login([FromBody] LoginModel model)
+        {
+            //Validation code
+
+            if (true)
+            {
+                FormsAuthentication.SetAuthCookie(model.Username, false);
+            }
+        }
+
+        [System.Web.Mvc.Authorize]
+        [System.Web.Mvc.HttpPost]
+        public void LogOut()
+        {
+            FormsAuthentication.SignOut();
+            //Response.Redirect("~/Views/Login/Index.cshtml");
+        }
+    }
+}
---
+++ BinaryOptions.WebServer/Scripts/Controllers/loginController.js
@@ -0,0 +1,23 @@
+﻿
+app.controller('loginController', ['$scope', '$http', function ($scope, $http) {
+
+    $scope.username = null;
+    $scope.password = null;
+
+    $scope.login = function() {
+        $http({
+            method: 'POST',
+            url: '/Login/Login',
+            data:
+            {
+                Username: $scope.username,
+                Password: $scope.password,
+            }
+        }).then(function successCallback(response) {
+            window.location.href = 'http://localhost:2641/';
+        }, function errorCallback(response) {
+            alertify.error("Login Failed, please try again.");
+        });
+    };
+
+}]);
